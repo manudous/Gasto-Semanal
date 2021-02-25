213e4d7e3be5200fcf6297158c0f030e00b48c7c
Manudous <manudous@gmail.com>
2021-02-25 17:58:51 +0100

    add Pregunta and Error

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
+import Pregunta from "./components/Pregunta";
 
 const App = () => {
   return (
     <div className="container">
-        <header>
-            <h1>Gasto Semanal</h1>
-        </header>
+      <header>
+        <h1>Gasto Semanal</h1>
+
+        <div className="contenido-principal contenido">
+          <Pregunta />
+        </div>
+      </header>
     </div>
-    )
+  );
 };
 
 export default App;
